9402bab114a6835f084036e519feca5849994699
Anjlnobita <jiteshkumar7200@gmail.com>
2024-11-22 15:06:19 +0530

    Update Telegram.py

--- Seistaxmusic/platforms/Telegram.py
+++ Seistaxmusic/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from AmritaXMusic import app
-from AmritaXMusic.utils.formatters import (
+from Siestaxmusic import app
+from Seistaxmusic.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
